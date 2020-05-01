f6c607656ecac7ada8f1c97c2ce38492d181ee22
Shuda Mo <msdtim@gmail.com>
2020-05-01 17:25:21 +0800

    chore: deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -22,12 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require('truffle-hdwallet-provider');
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+console.log(mnemonic);
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -50,6 +49,13 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/770da9b0b78d46d393ed365c502a894d`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
     },
 
     // Another network with more advanced options...
